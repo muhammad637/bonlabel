807432b0be8e9a302d3e6de746aa8d76d78530dc
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-03-21 08:52:16 +0700

    create table

--- database/migrations/2023_03_21_014141_create_ruangans_table.php
+++ database/migrations/2023_03_21_014141_create_ruangans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRuangansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username');
-            $table->string('password');
-            $table->string('nama');
-            $table->enum('level', ['admin', 'user']);
+        Schema::create('ruangans', function (Blueprint $table) {
+            $table->id('id_ruangan');
+            $table->string('nama_ruangan');
+            $table->enum('status', ['aktif', 'tidak aktif']);
             $table->string('no_telfon');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ruangans');
     }
 }
