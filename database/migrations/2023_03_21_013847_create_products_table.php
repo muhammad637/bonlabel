f6feb29a291ad6a26b5a12331caff03a3de1b7e7
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-03-21 08:53:55 +0700

    create table product

---
+++ database/migrations/2023_03_21_013847_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_product');
+            $table->string('jenis_product');
+            $table->string('limit_order');
+            $table->string('jumlah_stock');
+            $table->enum('status_product',['aktif','nonaktif']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
