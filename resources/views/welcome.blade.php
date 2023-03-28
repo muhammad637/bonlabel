76a709b2bf273a1fd4283257d0489afefaa00fba
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-03-28 13:25:18 +0700

    commit lagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,9 +46,6 @@
           <button type="submit" class="btn btn-primary">Login</button>
         </form>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
     </div>
   </div>
 </div>
