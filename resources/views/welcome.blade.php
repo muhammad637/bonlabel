6373d36cbdad4c0b14f5d43f8ddb8be01db04e0b
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-03-28 11:57:50 +0700

    ui landingpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,49 +6,46 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.1.1/swiper-bundle.min.js"></script>
     <link rel="stylesheet" href="assets/landingpage.css">
   </head>
   <body>
+   
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
     <div class="container">
-      <header class="masthead bg-gradient-primary-to-secondary">
-        <div class="container">
-          <div class="row align-items-center justify-content-center mr-2">
-            <div class="col-lg-5">
-              <!-- Mashead text and app badges-->
-              <div class="mt-5 mb-5 mb-lg-0 text-center text-lg-start">
-                <h1 class="display-1 lh-1 mb-3">
-                  Showcase your app beautifully.
-                </h1>
-                <p class="lead fw-normal text-muted mb-5">
-                  Launch your mobile app landing page faster with this free, open
-                  source theme from Start Bootstrap!
-                </p>
-                <div class="d-flex flex-column flex-lg-row align-items-center">
-                  <a class="me-lg-3 mb-4 mb-lg-0" href="#!"
-                    ><img
-                      class="app-badge"
-                      src="assets/img/google-play-badge.svg"
-                      alt="..."
-                  /></a>
-                  <a href="#!"
-                    ><img
-                      class="app-badge"
-                      src="assets/img/app-store-badge.svg"
-                      alt="..."
-                  /></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <!-- Masthead device mockup feature-->
-              <img src="../img/display.png" alt="#">
-              
-              </div>
-            </div>
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>SIBONLABEL</h1>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="" class="btn-get-started scrollto">Login</a>
           </div>
         </div>
-      </header>      
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="img/display.png" class="img-fluid animated" alt="">
+        </div>
       </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-  </body>
+    </div>
+
+  </section><!-- End Hero -->
+
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
 </html>
