9bd013b0ffe1bad18b6f6767079695d1a99cfa53
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-03-29 13:36:03 +0700

    commit to pull

--- resources/views/admin/parts/sidebar.blade.php
+++ resources/views/admin/parts/sidebar.blade.php
@@ -4,19 +4,19 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="/dashboardAdmin">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="/orderan">
           <i class="bi bi-cart"></i>
           <span>Orderan</span>
         </a>
       </li><!-- End Orderan Nav -->
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="#">
           <i class="bi bi-journals"></i>
           <span>Laporan</span>
         </a>
@@ -27,17 +27,17 @@
         </a>
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="components-alerts.html">
+            <a href="/user">
               <i class="bi bi-people"></i><span>User</span>
             </a>
           </li>
           <li>
-            <a href="components-alerts.html">
+            <a href="/product">
               <i class="bi bi-tag"></i><span>Product</span>
             </a>
           </li>
           <li>
-            <a href="components-alerts.html">
+            <a href="/ruangan">
               <i class="bi bi-house"></i><span>Ruangan</span>
             </a>
           </li>
