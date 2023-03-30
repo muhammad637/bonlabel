1bff39a661289728cf0dc49b0d61899699e5fc48
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-03-31 02:54:45 +0700

    fix bug

--- resources/views/admin/parts/navbar.blade.php
+++ resources/views/admin/parts/navbar.blade.php
@@ -8,17 +8,6 @@
               </a>
           </div>
           <!-- End Logo -->
-
-
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
             <li>
               <hr class="dropdown-divider">
             </li>
