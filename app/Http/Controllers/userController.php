cce6bfce1e845692748e51ba63d1b4a6315c8727
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-04-04 08:18:14 +0700

    merging fitur export to excel

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     {
         //
         return response(view('admin.pages.User.listUser', [
-            'users' => User::all(),
+            'users' => User::orderBy('created_at','desc')->get(),
         ]));
     }
 
