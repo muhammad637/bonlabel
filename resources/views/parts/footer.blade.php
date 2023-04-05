437c563aaca60bc2a9e17de3a2d91e6a719d3553
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-04-05 20:29:17 +0700

    footer

---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,64 @@
+<footer id="footer" class="mt-5">
+
+    <div class="footer-top">
+        <div class="px-5">
+            <div class="row" style="margin-bottom: 0;">
+                <div class="col-lg-8 col-md-6 footer-contact">
+                    <div class="row mb-5">
+                        <div class="col-md-6">
+                            <h3>SIBONLABEL</h3>
+                            <p><strong>Alamat: </strong>
+                                Jl. Letkol Istiqlah No.49, Singonegaran, Kec. Banyuwangi, Kabupaten Banyuwangi, Jawa
+                                Timur
+                                6841
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Contact Us</h4>
+                    <ul>
+                        <li><i class="bx"></i> <a href="#"><strong>Phone:<br></strong> (0333) 636780</a>
+                        </li>
+                        <li><i class="bx"></i> <a href="#"><strong>Email:<br></strong> poliwangi.ac.id</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container ">
+        <div class="copyright-wrap py-4 justify-content-start">
+            <div class="row">
+                <div class="col-md-8 col-sm-12 me-md-auto text-center text-md-start ps-md-5 my-2 ">
+                    <div class="copyright ">
+                        &copy; Copyright <strong><span>POLIWANGI</span></strong>. All Rights Reserved
+                    </div>
+                    <div class="credits">
+                        Designed by <a href="https://poliwangi.ac.id/">POLIWANGI STUDENT</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 mt-2">
+                    <div class="social-links text-center">
+                        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer><!-- End Footer -->
