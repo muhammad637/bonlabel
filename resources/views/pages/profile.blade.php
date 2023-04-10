f4fba026dcd580b032e33fa1f2b97562252411af
Aan-Triambudi <aan.argon123@gmail.com>
2023-04-10 12:07:29 +0700

    io

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -103,7 +103,7 @@
                     <div class="row mb-3">
                       <label for="cekLevel" class="col-md-4 col-lg-3 col-form-label">Level</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="cekLevel" type="text" class="form-control" id="cekLevel" placeholder="{{ auth()->user()->cekLevel }}" readonly>
+                        <input name="cekLevel" type="text" class="form-control" id="cekLevel" placeholder="{{ auth()->user()->cekLevel }}">
                       </div>
                     </div>
                     <div class="row mb-3">
@@ -174,49 +174,3 @@
         </div>
     </section>
 @endsection
-
-<!-- Vendor JS Files -->
-{{-- <script>
-    // membuat fungsi change
-    function change() {
-
-        // membuat variabel berisi tipe input dari id='pass', id='pass' adalah form input password 
-        var cp= document.getElementById('currentPassword').type;
-        var np= document.getElementById('newPassword').type;
-        var svg = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                                                            <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                                                            </svg>`;
-
-        //membuat if kondisi, jika tipe x adalah password maka jalankan perintah di bawahnya
-        if (cp == 'password'  ) {
-
-            //ubah form input password menjadi text
-           cp = 'text';
-
-            //ubah icon mata terbuka menjadi tertutup
-            document.getElementById('mybutton').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
-                                                            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
-                                                            <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
-                                                            </svg>`;
-        } else {
-
-            //ubah form input password menjadi text
-            document.getElementById('currentPassword').type = 'password';
-
-            //ubah icon mata terbuka menjadi tertutup
-            document.getElementById('mybutton').innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                                                            <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                                                            </svg>`;
-        }
-    }
-</script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
-<script>
-    $(document).ready( function(){
-        $('#iasd').html('simpan pinjam')
-    })
-</script> --}}
