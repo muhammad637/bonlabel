2370e4e6ad100a68698649bb696c2d22558d8593
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-05-06 07:28:01 +0700

    fitur konfirmasi password

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -178,7 +178,7 @@
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
-                                            @enderror
+                                            @enderror 
                                         </div>
                                     </div>
 
